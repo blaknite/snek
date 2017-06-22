4d699e7ae58e2c023fd1d51511872b4dbbeace11
Grant Colegate <blaknite@thelanbox.com.au>
2017-06-22 21:54:26 +0930

    Optimise locals and loops

--- snek.c
+++ snek.c
@@ -8,7 +8,7 @@
 #pragma output CLIB_STDIO_HEAP_SIZE = 0
 #pragma output CLIB_OPT_PRINTF = 0x1;
 
-#define SLEEP_CYCLES 2500
+#define CYCLES_PER_TICK 2500
 #define SCORE_PER_APPLE 10
 #define CYCLES_PER_APPLE 2
 #define COLOR_BG 40
@@ -19,13 +19,14 @@
 #define COLOR_END 31
 #define GRID_WIDTH 40
 #define GRID_HEIGHT 24
-#define GRID_EMPTY ' '
-#define GRID_WALL 'W'
-#define GRID_APPLE 'A'
+#define CELL_EMPTY ' '
+#define CELL_WALL 'W'
+#define CELL_APPLE 'A'
 #define DIR_UP 'U'
 #define DIR_DOWN 'D'
 #define DIR_LEFT 'L'
 #define DIR_RIGHT 'R'
+#define START_KEY ' '
 #define STATE_START 0
 #define STATE_RUN 1
 #define STATE_PAUSE 2
@@ -51,7 +52,12 @@ unsigned char grid[GRID_WIDTH * GRID_HEIGHT];
 unsigned int state = STATE_START;
 
 unsigned int score = 0;
-unsigned int sleep_cycles = SLEEP_CYCLES;
+unsigned int cycles_per_tick = CYCLES_PER_TICK;
+
+unsigned char direction = DIR_UP;
+
+unsigned int temp_x;
+unsigned int temp_y;
 
 unsigned int cell_index(unsigned int x, unsigned int y) {
   return y * GRID_WIDTH + x;
@@ -71,22 +77,19 @@ void draw_cell(unsigned int x, unsigned int y, unsigned int col) {
   rc2014_print("  ");
 }
 
-void init_grid() {
-  unsigned int x;
-  unsigned int y;
-
-  for ( y = 0; y < GRID_HEIGHT; ++y ) {
-    for ( x = 0; x < GRID_WIDTH; ++x ) {
-      if ( y == 0 || y == GRID_HEIGHT - 1 || x == 0 || x == GRID_WIDTH - 1 ) {
-        set_cell(x, y, CELL_WALL);
+void init_grid(void) {
+  for ( temp_y = 0; temp_y < GRID_HEIGHT; temp_y++ ) {
+    for ( temp_x = 0; temp_x < GRID_WIDTH; temp_x++ ) {
+      if ( temp_y == 0 || temp_y == GRID_HEIGHT - 1 || temp_x == 0 || temp_x == GRID_WIDTH - 1 ) {
+        set_cell(temp_x, temp_y, CELL_WALL);
       } else {
-        set_cell(x, y, CELL_EMPTY);
+        set_cell(temp_x, temp_y, CELL_EMPTY);
       }
     }
   }
 }
 
-void init_snake() {
+void init_snake(void) {
   snake_head.x = GRID_WIDTH / 2;
   snake_head.y = GRID_HEIGHT / 2;
   snake_head.direction = DIR_UP;
@@ -100,50 +103,49 @@ void init_snake() {
   set_cell(snake_head.x, snake_head.y + 2, snake_head.direction);
 }
 
-void init_apple() {
+void init_apple(void) {
   apple.x = 0;
   apple.y = 0;
 }
 
 void set_direction(char key) {
+  if ( direction != snake_head.direction ) return;
+
   switch ( key ) {
     case 'w':
     case 'W':
-      if ( snake_head.direction != DIR_DOWN ) snake_head.direction = DIR_UP;
+      if ( direction != DIR_DOWN ) direction = DIR_UP;
       break;
 
     case 'a':
     case 'A':
-      if ( snake_head.direction != DIR_RIGHT ) snake_head.direction = DIR_LEFT;
+      if ( direction != DIR_RIGHT ) direction = DIR_LEFT;
       break;
 
     case 's':
     case 'S':
-      if ( snake_head.direction != DIR_UP ) snake_head.direction = DIR_DOWN;
+      if ( direction != DIR_UP ) direction = DIR_DOWN;
       break;
 
     case 'd':
     case 'D':
-      if ( snake_head.direction != DIR_LEFT ) snake_head.direction = DIR_RIGHT;
+      if ( direction != DIR_LEFT ) direction = DIR_RIGHT;
       break;
   }
 }
 
-void new_apple() {
-  while ( get_cell(apple.x, apple.y) != GRID_EMPTY ) {
+void new_apple(void) {
+  while ( get_cell(apple.x, apple.y) != CELL_EMPTY ) {
     apple.x = rand() % GRID_WIDTH;
     apple.y = rand() % GRID_HEIGHT;
   }
 
-  set_cell(apple.x, apple.y, GRID_APPLE);
+  set_cell(apple.x, apple.y, CELL_APPLE);
 }
 
-void end() {
-  state = STATE_END;
-  rc2014_ansi_cls();
-}
+void update_snake(void) {
+  snake_head.direction = direction;
 
-void update_snake() {
   set_cell(snake_head.x, snake_head.y, snake_head.direction);
 
   switch ( snake_head.direction ) {
@@ -167,14 +169,25 @@ void update_snake() {
   }
 }
 
-void update_score() {
-  if ( apple.x != snake_head.x || apple.y != snake_head.y ) return;
-  new_apple();
-  score += SCORE_PER_APPLE;
-  sleep_cycles -= CYCLES_PER_APPLE;
+void end(void) {
+  state = STATE_END;
+  rc2014_ansi_cls();
 }
 
-void draw_start() {
+void update_score() {
+  unsigned char val = get_cell(snake_head.x, snake_head.y);
+
+  if ( val != CELL_APPLE && val != CELL_EMPTY ) end();
+
+  if ( apple.x != snake_head.x || apple.y != snake_head.y ) return;
+
+  new_apple();
+
+  score += SCORE_PER_APPLE;
+  cycles_per_tick -= CYCLES_PER_APPLE;
+}
+
+void draw_start(void) {
   rc2014_ansi_color(31);
   rc2014_ansi_move_cursor(GRID_HEIGHT / 2 - 6, GRID_WIDTH - 12);
   rc2014_print("####  #   #  ####  #  #");
@@ -200,55 +213,49 @@ void draw_start() {
   rc2014_print("--| PRESS SPACE TO START |--");
 }
 
-void draw_grid() {
-  unsigned int x;
-  unsigned int y;
-
-  rc2014_ansi_color(COLOR_BG);
-  rc2014_ansi_cls();
-
-  for ( y = 0; y < GRID_HEIGHT; ++y ) {
-    for ( x = 0; x < GRID_WIDTH; ++x ) {
-      switch ( get_cell(x, y) ) {
-        case GRID_WALL:
-          draw_cell(x, y, COLOR_WALL);
+void draw_grid(void) {
+  for ( temp_y = 0; temp_y < GRID_HEIGHT; temp_y++ ) {
+    for ( temp_x = 0; temp_x < GRID_WIDTH; temp_x++ ) {
+      switch ( get_cell(temp_x, temp_y) ) {
+        case CELL_WALL:
+          draw_cell(temp_x, temp_y, COLOR_WALL);
           break;
 
-        case GRID_APPLE:
-          draw_cell(x, y, COLOR_APPLE);
+        case CELL_APPLE:
+          draw_cell(temp_x, temp_y, COLOR_APPLE);
           break;
 
         case DIR_UP:
         case DIR_DOWN:
         case DIR_LEFT:
         case DIR_RIGHT:
-          draw_cell(x, y, COLOR_SNAKE);
+          draw_cell(temp_x, temp_y, COLOR_SNAKE);
           break;
       }
     }
   }
 }
 
-void draw_snake() {
-  unsigned int x = snake_tail.x;
-  unsigned int y = snake_tail.y;
+void draw_snake(void) {
+  temp_x = snake_tail.x;
+  temp_y = snake_tail.y;
 
   switch ( snake_tail.direction ) {
-    case DIR_UP:    y++; break;
-    case DIR_DOWN:  y--; break;
-    case DIR_LEFT:  x++; break;
-    case DIR_RIGHT: x--; break;
+    case DIR_UP:    temp_y++; break;
+    case DIR_DOWN:  temp_y--; break;
+    case DIR_LEFT:  temp_x++; break;
+    case DIR_RIGHT: temp_x--; break;
   }
 
   draw_cell(snake_head.x, snake_head.y, COLOR_SNAKE);
-  draw_cell(x, y, COLOR_BG);
+  draw_cell(temp_x, temp_y, COLOR_BG);
 }
 
-void draw_apple() {
+void draw_apple(void) {
   draw_cell(apple.x, apple.y, COLOR_APPLE);
 }
 
-void draw_score() {
+void draw_score(void) {
   unsigned char str[14];
 
   sprintf(str, "SCORE: %d", score);
@@ -258,36 +265,36 @@ void draw_score() {
   rc2014_print(str);
 }
 
-void draw_pause() {
+void draw_pause(void) {
   rc2014_ansi_color(COLOR_BG);
   rc2014_ansi_color(COLOR_TEXT);
   rc2014_ansi_move_cursor(GRID_HEIGHT + 1, 1);
   rc2014_print("PAUSED        ");
 }
 
-void draw_help() {
+void draw_help(void) {
   rc2014_ansi_color(COLOR_BG);
   rc2014_ansi_color(COLOR_TEXT);
-  rc2014_ansi_move_cursor(GRID_HEIGHT + 1, 38);
-  rc2014_print("W: Up  S: Down  A: Left  D: Right  P: Pause");
+  rc2014_ansi_move_cursor(GRID_HEIGHT + 1, 34);
+  rc2014_print("W: Up  S: Down  A: Left  D: Right  Space: Pause");
 }
 
-void draw_end() {
-  unsigned char str[20];
+void draw_end(void) {
+  unsigned char str[12];
 
   rc2014_ansi_color(COLOR_END);
   rc2014_ansi_move_cursor(GRID_HEIGHT / 2 - 2, GRID_WIDTH - 5);
   rc2014_print("GAME OVER!");
-  rc2014_ansi_move_cursor(GRID_HEIGHT / 2, GRID_WIDTH - 8);
-  sprintf(str, "FINAL SCORE: %d", score);
+  rc2014_ansi_move_cursor(GRID_HEIGHT / 2, GRID_WIDTH - 5);
+  sprintf(str, "SCORE: %d", score);
   rc2014_print(str);
   rc2014_ansi_move_cursor(GRID_HEIGHT / 2 + 2, GRID_WIDTH - 14);
   rc2014_print("--| PRESS SPACE TO START |--");
 }
 
-void start() {
+void start(void) {
   score = 0;
-  sleep_cycles = SLEEP_CYCLES;
+  cycles_per_tick = CYCLES_PER_TICK;
 
   state = STATE_RUN;
 
@@ -304,48 +311,43 @@ void start() {
   draw_help();
 }
 
-void input() {
-  unsigned int i = sleep_cycles;
+void input(void) {
   unsigned char key;
 
-  while ( i-- ) {
-    if ( !rc2014_uart_rx_ready() ) continue;
+  if ( !rc2014_uart_rx_ready() ) return;
 
-    key = rc2014_uart_rx();
+  key = rc2014_uart_rx();
 
-    switch ( state ) {
-      case STATE_START:
-      case STATE_END:
-        if ( key == ' ' ) start();
-        break;
-
-      case STATE_RUN:
-        set_direction(key);
-        if ( key == 'p' ) state = STATE_PAUSE;
-        break;
-
-      case STATE_PAUSE:
-        if ( key == 'p' ) state = STATE_RUN;
-        break;
-    }
-  }
-}
-
-void update() {
   switch ( state ) {
     case STATE_START:
     case STATE_END:
-      rand();
+      if ( key == START_KEY ) start();
       break;
 
+    case STATE_RUN:
+      set_direction(key);
+      if ( key == START_KEY ) state = STATE_PAUSE;
+      break;
+
+    case STATE_PAUSE:
+      if ( key == START_KEY ) state = STATE_RUN;
+      break;
+  }
+}
+
+void update(void) {
+  switch ( state ) {
     case STATE_RUN:
       update_snake();
       update_score();
       break;
+
+    default:
+      rand();
   }
 }
 
-void draw() {
+void draw(void) {
   switch ( state ) {
     case STATE_START:
       draw_start();
@@ -367,14 +369,17 @@ void draw() {
   }
 }
 
-// Main game loop
-void main() {
+void main(void) {
+  static unsigned int i;
+
   rc2014_ansi_hide_cursor();
   rc2014_ansi_color(COLOR_BG);
   rc2014_ansi_cls();
 
   while ( 1 ) {
-    input();
+    i = cycles_per_tick;
+
+    while ( i-- ) input();
     update();
     draw();
   }
