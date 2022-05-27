29b42a5ee70fe7d88b18cc8bc2a92eb21483b8b7
Grant Colegate <blaknite@thelanbox.com.au>
2022-05-27 11:28:42 +0930

    Tidy up asm

--- snek.asm
+++ snek.asm
@@ -1,18 +1,18 @@
 SECTION code_user
 
-EXTERN asm_cpm_bdos
+defc cpm_bdos = 5
 
 ; Immediately get a character from the console
 public _getchar_noblock
 _getchar_noblock:
-            ld hl, 0
-            ld c, 6
-            ld e, 0FFh
-            call asm_cpm_bdos
-            ld l, a
-            ret
+  ld c, 6 ; CP/M direct console I/O
+  ld e, $FF
+  call cpm_bdos
+  ld h, 0
+  ld l, a
+  ret
 
 ; Quit and return to command prompt
 public _quit
 _quit:
-            rst 0
+  rst 0
